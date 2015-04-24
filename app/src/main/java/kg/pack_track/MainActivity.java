b80cb84b0e616a8204c5403f9aeda8625667c53d
borogarr <kgonzalez99@ufl.edu>
2015-04-24 12:21:39 -0400

    added basic layout and filler function

--- app/src/main/java/kg/pack_track/MainActivity.java
+++ app/src/main/java/kg/pack_track/MainActivity.java
@@ -4,36 +4,61 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends ActionBarActivity {
+
+    private ArrayAdapter<String> packsAdapter;
+    private ArrayList<String> packs;
+    private ListView lvPacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvPacks = (ListView) findViewById(R.id.lvPacks);
+        packs = new ArrayList<String>();
+        //add file storing functionality
+        packsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, packs);
+        lvPacks.setAdapter(packsAdapter);
+        setupListViewListener();
+
+    }
+
+    private void setupListViewListener() {
+        lvPacks.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                        packs.remove(position);
+                        packsAdapter.notifyDataSetChanged();
+                        //add write to file function call
+                        return true;
+                    }
+                }
+        );
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onAddPack(View v) {
+        EditText etNewPack = (EditText) findViewById(R.id.etNewPack);
+        String packText = etNewPack.getText().toString();
+        //Add package regex stuff here - function calls / etc..
+        packsAdapter.add(packText);
+        etNewPack.setText("");
+        //add write item function call
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
